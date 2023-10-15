eaab3e88b48dce6c1a456f513a23e0c9d92efb99
ikhwanuddin39 <ikhwanuddin039@gmail.com>
2023-10-15 11:38:11 +0700

    update api services

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -29,4 +29,44 @@ export class ApiService<T> {
         })
       );
   }
+
+  getById(id: string): Observable<T> {
+    return this.http.get<T>(`${this.url}/${this.endpoint}/${id}`)
+      .pipe(
+        catchError(error => {
+          console.error('Error:', error);
+          return [];
+        })
+      );
+  }
+
+  insert(data: T): Observable<T> {
+    return this.http.post<T>(`${this.url}/${this.endpoint}`, data)
+      .pipe(
+        catchError(error => {
+          console.error('Error:', error);
+          return [];
+        })
+      );
+  }
+
+  updateById(id: string, data: T): Observable<T> {
+    return this.http.put<T>(`${this.url}/${this.endpoint}/${id}`, data)
+      .pipe(
+        catchError(error => {
+          console.error('Error:', error);
+          return [];
+        })
+      );
+  }
+  // delete by id
+  delete(id: string): Observable<T> {
+    return this.http.delete<T>(`${this.url}/${this.endpoint}/${id}`)
+      .pipe(
+        catchError(error => {
+          console.error('Error:', error);
+          return [];
+        })
+      );
+  }
 }
